e4064be72bb577342d9b4c35d9ef2aa8911624d7
fernandoaguiar95 <62913150+fernandoaguiar95@users.noreply.github.com>
2021-06-06 17:45:43 -0300

    1
    versão 1

---
+++ app/src/main/java/com/example/layoutpintor/CadastraCliente.kt
@@ -0,0 +1,11 @@
+package com.example.layoutpintor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastraCliente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_cliente)
+    }
+}
